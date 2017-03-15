9c2dafc01bec1e803c339d419f847d8bb5b64986
Sean Perez <sean.perez29@gmail.com>
2017-03-14 20:14:34 -0600

    Initial Commit

---
+++ ClassAnatomy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClassAnatomy
+//
+//  Created by Sean Perez on 3/14/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClassAnatomy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ClassAnatomy
+//
+//  Created by Sean Perez on 3/14/17.
+//  Copyright © 2017 SeanPerez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
